ab4bbefb3b0b13a7688c2bc71bc27727d8b7a59e
Joel-Olutoye <donronald27@gmail.com>
2022-08-05 09:27:37 +0100

    Edits

---
+++ src/components/Newslist.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect} from 'react'
+
+const Newslist = () => {
+    const [articles, setArticles] = useState([])
+
+    useEffect(()=>{
+
+    }, [])
+  return (
+    <div>Newslist</div>
+  )
+}
+
+export default Newslist
